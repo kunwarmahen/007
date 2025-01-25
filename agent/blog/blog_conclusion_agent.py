185ad0b506aac1c5c88605f0441ab527a0012b6f
Mahen Singh <kunwarmahen@gmail.com>
2025-01-24 21:10:02 -0500

    Changed BlogAgents to extend BaseAgent

--- agent/blog/blog_conclusion_agent.py
+++ agent/blog/blog_conclusion_agent.py
@@ -1,34 +1,11 @@
 import json
-from typing import Dict
 from util.time_exe import time_execution
-from llm.base.llmclient import BaseLLMClient
-from llm.base.llmclient import ChatClient
+from agent.base.base_agent import BaseAgent
 
-class BlogConclusionAgent:
+class BlogConclusionAgent(BaseAgent):
     def __init__(self, base_url="http://localhost:11434", model="qwen2.5:32b", temperature=0.0, stream=False):
-        
-        """Initialize Agent with a base url and model name."""
-        self.llamaclient = BaseLLMClient(base_url=base_url, model=model, temperature=temperature, stream=stream, system_prompt_func=self.create_conclusion_prompt)
-        self.client = ChatClient(self.llamaclient)
-        
-
-    def call_llm(self, user_query: str) -> Dict:
-        """Call the LLM to generate a response."""
-
-        messages = [
-            {"role": "user", "content": user_query}
-        ]
-        
-        # Chat with the API
-        response = self.client.chat(messages)        
-                
-        # Prepare the request to Ollama        
-        json_response = response.json()
-        
-        try:
-            return json.loads( json_response['message']['content'])
-        except json.JSONDecodeError:
-            raise ValueError("Failed to parse LLM response as JSON")
+        """Initialize Agent the agent."""
+        super().__init__(base_url, model, temperature, stream, system_prompt_func=self.create_conclusion_prompt)
 
     @time_execution        
     def execute(self, user_query: str) -> str:
